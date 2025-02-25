a36e1d624267824a4828223b76cb86876748330f
isaactavares <pablomoraisie@gmail.com>
2025-02-25 12:08:14 +0000

    exercicio 14

---
+++ ex014.py
@@ -0,0 +1,3 @@
+temp = float(input('Informe a temperatura em ºC: '))
+far = (temp * 1.8) + 32 # essa é uma das fórmulas para chegar em Fahrenheit
+print('A temperatura de {}ºC corersponde a {}ºF!'.format(temp, far))
