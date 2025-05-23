5d062cec6dda9aa61e3132039e01054e64548a92
isaactavares <pablomoraisie@gmail.com>
2025-05-23 12:16:43 +0100

    exs

---
+++ ex059.py
@@ -0,0 +1,34 @@
+from time import sleep
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+opção = 0
+while opção != 5:
+    print('''    [ 1 ] Somar
+    [ 2 ] Multiplicar
+    [ 3 ] Mostrar maior
+    [ 4 ] Novos Números 
+    [ 5 ] Sair do Programa''')
+    opção = int(input('>>>>>>>> Qual a sua opção? '))
+    if opção == 1:
+        soma = n1 + n2
+        print('A soma entre {} + {} é {}.'.format(n1, n2, soma))
+    elif opção == 2:
+        mult = n1 * n2
+        print('O produto entre {} * {} é {}.'.format(n1, n2, mult))
+    elif opção == 3:
+        if n1 > n2:
+            maior = n1
+        else:
+            maior = n2
+        print(' O maior número entre {} e {} é {}.'.format(n1, n2, maior))
+    elif opção == 4:
+        n1 = int(input('Digite o primeiro valor: '))
+        n2 = int(input('Digite o segundo valor: '))
+    elif opção == 5:
+        print('Finalizando...')
+    else:
+        print('Opção inválida. Tente novamente.')
+    print ('=-=' * 10)
+    sleep(1.5)
+print('Fim do Programa. Volte Sempre!')
+
