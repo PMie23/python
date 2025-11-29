7973a4f6d89f9bb84ff13fbd89bc7b39da287965
isaactavares <pablomoraisie@gmail.com>
2025-11-29 10:48:55 +0000

    finalizando com erro

--- projeto_database_python/database_project.py
+++ projeto_database_python/database_project.py
@@ -1,4 +1,4 @@
-import matplotlib
+import matplotlib                   # Matplotlib needs this import to visualize the graphs
 matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
@@ -13,6 +13,26 @@ df = pd.read_csv(r"C:\Users\Isaac Tavares\Documents\MeuPC\python\projeto_databas
 # Data visualization
 
 print(df)
+
+# Which habits have the biggest impact on students performance?
+# Filtering numeric columns
+columns = [
+    "study_hours_per_day",
+    "social_media_hours",
+    "netflix_hours",
+    "sleep_hours",
+    "attendance_percentage",
+    "exercise_frequency",
+    "mental_health_rating",
+    "exam_score",
+]
+# Plot "heatmap"
+plt.title("Correlation between habits and exam score")
+plt.show()
+
+
+
+
 
 
 
