339129bc8183a5da78e5aa90b1d0ea70e12bc99b
isaactavares <pablomoraisie@gmail.com>
2025-11-29 10:39:53 +0000

    Inicio no pycharm

--- projeto_database_python/database_project.py
+++ projeto_database_python/database_project.py
@@ -6,6 +6,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+# Importing and learning about database
+
+df = pd.read_csv(r"C:\Users\Isaac Tavares\Documents\MeuPC\python\projeto_database_python\student_habits_performance.csv")
+
+# Data visualization
+
+print(df)
+
+
 
 
 
